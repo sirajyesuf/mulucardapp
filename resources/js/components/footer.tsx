6c8972a65d16bb9b6cadf3e7d3b36c2ad966a845
Siraj Yesuf <sirajyesuf762@gmail.com>
2025-03-12 17:57:27 +0300

    =simple footer for dashboard

--- resources/js/components/footer.tsx
+++ resources/js/components/footer.tsx
@@ -1,24 +1,33 @@
 import { Link } from '@inertiajs/react';
-import { Facebook } from 'lucide-react';
+import { Facebook, Instagram, Twitter } from 'lucide-react';
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center justify-center gap-4 border-t-2 p-2">
-            <div className="mx-auto flex w-xl flex-row items-center justify-between border-none">
+        <footer className="flex flex-col items-center justify-center gap-2 border-t-2 py-4">
+            <div className="flex flex-row items-center justify-between gap-8 border-none p-2 font-bold">
                 <Link href="home">Home</Link>
-                <Link href="home">Home</Link>
-                <Link href="home">Home</Link>
-                <Link href="home">Home</Link>
-                <Link href="home">Home</Link>
-                <Link href="home">Home</Link>
+                <Link href="home">Blog</Link>
+                <Link href="home">Privacy Policy</Link>
+                <Link href="home">terms</Link>
             </div>
 
             <div className="flex flex-row items-center justify-center gap-4 border-none p-0">
-                <Facebook size={50} className="rounded-full border-2 bg-white p-2" />
-                <Facebook size={50} className="rounded-full border-2 bg-white p-2" />
-                <Facebook size={50} className="rounded-full border-2 p-2" />
-                <Facebook size={50} className="rounded-full border-2 bg-white p-2" />
-                <Facebook size={50} className="rounded-full border-2 bg-white p-2" />
-                <Facebook size={50} className="rounded-full border-2 bg-white p-2" />
+                <div className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-[#ff8c39]">
+                    <a href="/" className="text-xl font-bold text-black">
+                        <Facebook className="h-5 w-5 text-white" />
+                    </a>
+                </div>
+
+                <div className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-[#ff8c39]">
+                    <a href="/" className="text-xl font-bold text-black">
+                        <Twitter className="h-5 w-5 text-white" />
+                    </a>
+                </div>
+
+                <div className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-[#ff8c39]">
+                    <a href="/" className="text-xl font-bold text-black">
+                        <Instagram className="h-5 w-5 text-white" />
+                    </a>
+                </div>
             </div>
         </footer>
     );
